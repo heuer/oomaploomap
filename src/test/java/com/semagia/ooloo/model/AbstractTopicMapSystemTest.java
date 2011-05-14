96bd2f7743af2202fbd61f4da881494ebee9e4c0
heuer <heuer@semagia.com>
2011-05-14 16:44:09 +0200

    More tests

--- src/test/java/com/semagia/ooloo/model/AbstractTopicMapSystemTest.java
+++ src/test/java/com/semagia/ooloo/model/AbstractTopicMapSystemTest.java
@@ -15,6 +15,11 @@
  */
 package com.semagia.ooloo.model;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
 import com.semagia.ooloo.model.ITopicMapSystem.ITopicMapSource;
 import com.semagia.ooloo.query.QueryLanguage;
 
@@ -59,6 +64,48 @@ public abstract class AbstractTopicMapSystemTest extends TestCase {
         final ITopicMapSource[] sources = _sys.getTopicMapSources();
         assertNotNull(sources);
         assertTrue(sources.length == 0);
+    }
+
+    public void testImportIllegal() throws IOException {
+        try {
+            _sys.loadSource(null);
+            fail("Expected an IAE");
+        }
+        catch (IllegalArgumentException ex) {
+            // noop.
+        }
+    }
+
+    public void testImport() throws IOException, URISyntaxException {
+        ITopicMapSource[] sources = _sys.getTopicMapSources();
+        assertTrue(sources.length == 0);
+        final URL url = AbstractTopicMapSystemTest.class.getResource("/test-tm.ctm");
+        assertNotNull(url);
+        final URI uri = url.toURI();
+        final ITopicMapSource src = _sys.loadSource(uri);
+        assertNotNull(src);
+        assertEquals(uri, src.getURI());
+        assertNull(src.getName());
+        sources = _sys.getTopicMapSources();
+        assertTrue(sources.length == 1);
+        assertEquals(sources[0].getURI(), src.getURI());
+        assertEquals(sources[0].getName(), src.getName());
+    }
+
+    public void testImport2() throws IOException, URISyntaxException {
+        ITopicMapSource[] sources = _sys.getTopicMapSources();
+        assertTrue(sources.length == 0);
+        final URL url = AbstractTopicMapSystemTest.class.getResource("/test-tm-with-name.ctm");
+        assertNotNull(url);
+        final URI uri = url.toURI();
+        final ITopicMapSource src = _sys.loadSource(uri);
+        assertNotNull(src);
+        assertEquals(uri, src.getURI());
+        assertEquals("Name", src.getName());
+        sources = _sys.getTopicMapSources();
+        assertTrue(sources.length == 1);
+        assertEquals(sources[0].getURI(), src.getURI());
+        assertEquals(sources[0].getName(), src.getName());
     }
 
 }
